3c0d98c50153bda45e988ad97ce3249914d09a0b
Rafael Pinto <archlinuxrp@gmail.com>
2022-05-31 17:44:41 +0100

    improve error messages

--- src/lib/utils/messages.ts
+++ src/lib/utils/messages.ts
@@ -1,43 +1,61 @@
+enum FormatInvalidTypeEnum {
+	'numeric' = 'number',
+	'alpha' = 'text',
+	'alphanum' = 'alpha numeric',
+	'alphadash' = 'alpha dash',
+	'hexadecimal' = 'hexadecimal',
+	'hex' = 'hex',
+	'base64' = 'base64',
+	'uuid' = 'uuid',
+	'cuid' = 'cuid',
+	'url' = 'url',
+	'singleLine' = 'single line',
+	'email' = 'email',
+}
+
+const getInvalidFormatMessage = (type: keyof typeof FormatInvalidTypeEnum) => {
+	return `Invalid ${FormatInvalidTypeEnum[type]} format`
+}
 
 export const defaultMessages = {
 	array: {
-		empty: 'Needs to be empty',
+		empty: 'Requires to be empty',
 		min: (minValue: number) => minValue === 1 ? `Requires at least ${minValue} item` : `Requires at least ${minValue} items`,
-		max: (maxValue: number) => maxValue === 1 ? `Requires max ${maxValue} item` : `Requires max ${maxValue} items`,
-		length: (length: number) => length === 1 ? `Requires the ${length} item` : `Requires the ${length} items`,
-		unique: 'Value is not unique',
-		uniqueBy: 'Value is not unique'
+		max: (maxValue: number) => maxValue === 1 ? `Requires maximum of ${maxValue} item` : `Requires maximum of ${maxValue} items`,
+		length: (length: number) => length === 1 ? `Requires ${length} item` : `Requires ${length} items`,
+		unique: 'Requires to be unique',
+		uniqueBy: 'Requires to be unique'
 	},
 	date: {
-		today: 'Is not today\'s date'
+		today: 'Requires to be today\'s date'
 	},
 	number: {
-		min: (minValue: number) => `Requires value to be at least ${minValue}`,
-		max: (maxValue: number) => `Requires value to be max ${maxValue}`,
-		equals: (equalsValue: number) => `Requires value to ${equalsValue}`,
-		integer: 'Requires value to not have decimal',
-		decimal: (equalsValue: number) => `Requires value to have ${equalsValue} decimal`,
-		positive: 'Requires value to be positive',
-		negative: 'Requires value to be negative'
+		min: (minValue: number) => `Requires to be at least ${minValue}`,
+		max: (maxValue: number) => `Requires to be maximum of ${maxValue}`,
+		equals: (equalsValue: number) => `Needs to be the same as ${equalsValue}`,
+		integer: 'Requires to be a integer number',
+		decimal: (equalsValue: number) => `Requires to have ${equalsValue} decimal number`,
+		positive: 'Requires to be positive number',
+		negative: 'Requires to be negative number'
 	},
 	string: {
-		min: (minValue: number) => `Requires string to have at least size ${minValue}`,
-		max: (maxValue: number) => `Requires string to have max size ${maxValue}`,
+		min: (minValue: number) => `Requires to have at least minimum size of ${minValue}`,
+		max: (maxValue: number) => `Requires to have maximum size of ${maxValue}`,
 		length: (equalsValue: number) => `Requires string to have size ${equalsValue}`,
 		pattern: (reg: RegExp) => `Requires string to match ${reg.toString()}`,
 		empty: 'Requires string to be empty',
-		contains: (value: string) => `Requires string to contain ${value}`,
-		numeric: 'Requires string to be numeric',
-		alpha: 'Requires string to be alpha',
-		alphanum: 'Requires string to be alpha-numeric',
-		alphadash: 'Requires string to be alpha-dash',
-		hex: 'Requires string to be hexadecimal',
-		base64: 'Requires string to be base64',
-		uuid: 'Requires string to be format (00000000-0000-0000-0000-000000000000)',
-		cuid: 'Requires string to be format cuid',
-		url: 'Requires string to be format url',
-		singleLine: 'Requires string to be in single line',
-		email: 'Requires string to be format email'
+		contains: (value: string) => `Requires ${value}`,
+		numeric: getInvalidFormatMessage('numeric'),
+		alpha: getInvalidFormatMessage('alpha'),
+		alphanum: getInvalidFormatMessage('alphanum'),
+		alphadash: getInvalidFormatMessage('alphadash'),
+		hex: getInvalidFormatMessage('hexadecimal'),
+		base64: getInvalidFormatMessage('base64'),
+		uuid: getInvalidFormatMessage('uuid'),
+		cuid: getInvalidFormatMessage('cuid'),
+		url: getInvalidFormatMessage('url'),
+		singleLine: getInvalidFormatMessage('singleLine'),
+		email: getInvalidFormatMessage('email')
 	},
 	notOptional: 'Not optional item',
 	notNullable: 'Not optional item',
